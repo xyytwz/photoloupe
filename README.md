83aaa4974fa2e1e9d38f1b27851eea3dfa0442c0
xyytwz <31400187+xyytwz@users.noreply.github.com>
2018-04-04 14:40:51 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # photoloupe
-VUE图片放大镜（兼容IE9）
+
+> 图片放大器
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+      <div class="goodPic">
+          <photoloupe class :src="goodPic" :magnification="10"></photoloupe>
+      </div>
+  </div>
+</template>
+
+<script>
+import goodPic from "./assets/godPic1.jpg";
+import photoloupe from './components/photoloupe'
+export default {
+  name: 'app',
+  components:{
+      photoloupe
+  },
+  data () {
+    return {
+      goodPic:goodPic
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top:30px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+.goodPic{
+    width:400px;
+    height:400px;
+    border:1px solid #ddd;
+}
+</style>
